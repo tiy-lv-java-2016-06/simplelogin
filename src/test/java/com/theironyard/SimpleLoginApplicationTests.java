ebbabf19c8119d6147f474c4093a137142d855d5
Jeff <jeff@goproject100.com>
2016-08-03 10:18:26 -0700

    Adding a test

--- src/test/java/com/theironyard/SimpleLoginApplicationTests.java
+++ src/test/java/com/theironyard/SimpleLoginApplicationTests.java
@@ -1,16 +1,68 @@
 package com.theironyard;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.theironyard.command.UserCommand;
+import com.theironyard.entities.User;
+import com.theironyard.services.UserRepository;
+import com.theironyard.utilities.PasswordStorage;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.junit.Assert.assertTrue;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SimpleLoginApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+	@Autowired
+    WebApplicationContext wac;
+
+    @Autowired
+    UserRepository userRepository;
+
+    MockMvc mockMvc;
+
+    @Before
+    public void before(){
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+    }
+
+    @Test
+    public void whenTokenGoodInfoThenReturnToken() throws Exception {
+        User user = new User("eddy", PasswordStorage.createHash("password"));
+        userRepository.save(user);
+
+        UserCommand userCommand = new UserCommand();
+        userCommand.setUsername(user.getUsername());
+        userCommand.setPassword("password");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(userCommand);
+
+
+        MvcResult result = mockMvc.perform(
+                MockMvcRequestBuilders.post("/token")
+                .content(json)
+                .contentType("application/json")
+        ).andExpect(status().isOk()).andReturn();
+
+    }
+
+    @Test
+    public void fakeTest(){
+        assertTrue(true);
+    }
 
 }
